51617a4709df10ef85f7868ce9ca503a9fa5f348
charliehuge <charlie.huguenard@gmail.com>
2011-02-12 19:23:25 -0800

    Added Conversation Engine Files

---
+++ ConversationEngine/Conversation.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace ConversationEngine
+{
+    //---------------------------------------------------------------------------------------
+    //
+    // Conversation
+    //  TODO: Many Comments
+    //
+    //---------------------------------------------------------------------------------------
+    public class Conversation : DrawableGameComponent
+    {
+        private List<string> Keybinds = new List<string>()
+        {
+            "A",
+            "S",
+            "D",
+            "F"
+        };
+
+        SpriteBatch spriteBatch;
+        Rectangle screenBounds;
+        SpriteFont font;
+        DialogTree dialogTree;
+        DialogNode currentNode;
+        DialogNode nextNode;
+        double timeSinceNodeSelect;
+
+        public Conversation(Game game, ref InputManager inputManager, SpriteBatch spriteBatch)
+            : base(game)
+        {
+            game.Components.Add(this);
+
+            screenBounds = game.GraphicsDevice.Viewport.Bounds;
+            
+            inputManager.KB_Any += new InputManager.KBEvent(OnKB);
+
+            this.spriteBatch = spriteBatch;
+
+            font = game.Content.Load<SpriteFont>("Segoe");
+
+            dialogTree = new DialogTree("data/CharlieTree.xml");
+            currentNode = dialogTree.GetFirstNode();
+
+            timeSinceNodeSelect = 0.0;
+        }
+
+        private void OnKB(KeyboardState state)
+        {
+            if (state.IsKeyDown(Keys.A))
+                SelectResponse("A");
+            else if (state.IsKeyDown(Keys.S))
+                SelectResponse("S");
+            else if (state.IsKeyDown(Keys.D))
+                SelectResponse("D");
+            else if (state.IsKeyDown(Keys.F))
+                SelectResponse("F");
+        }
+
+        private void SelectResponse(string keyPressed)
+        {
+            if (currentNode == null || currentNode.responses == null) return;
+
+            int responseSelected = -1;
+            for(int i = 0; i < Keybinds.Count; i++)
+            {
+                if (Keybinds[i] == keyPressed)
+                    responseSelected = i;
+            }
+            if (responseSelected == -1 || responseSelected >= currentNode.responses.Count) return;
+
+            nextNode = dialogTree.GetNodeByName(currentNode.responses[responseSelected].nextNode);
+            currentNode = null;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (nextNode != null)
+            {
+                timeSinceNodeSelect += gameTime.ElapsedGameTime.TotalSeconds;
+                if (timeSinceNodeSelect > 0.3)
+                {
+                    currentNode = nextNode;
+                    currentNode.NextText();
+                    DoDialogAction();
+                    nextNode = null;
+                    timeSinceNodeSelect = 0.0;
+                }
+            }
+        }
+
+        private void DoDialogAction()
+        {
+            if (currentNode.action == null) return;
+
+            string action = currentNode.action.name;
+            List<string> actionParams = currentNode.action.strParams;
+
+            if (action == "music")
+            {
+                Console.WriteLine("playing music: " + actionParams[0]);
+            }
+            else if (action == "sfx")
+            {
+                Console.WriteLine("playing sfx: " + actionParams[0]);
+            }
+            else
+            {
+                Console.WriteLine("action not recognized: " + action + " param1: " + actionParams[0]);
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            DrawDialogNode();
+            spriteBatch.DrawString(font, "[Esc] to Exit", new Vector2(0f, screenBounds.Height - font.LineSpacing), Color.White);
+            spriteBatch.End();
+        }
+
+        private void DrawDialogNode()
+        {
+            if (currentNode == null) return;
+
+            string nodeText = WrapText(font, currentNode.GetCurrentText(), screenBounds.Width);
+            spriteBatch.DrawString(font, nodeText, Vector2.Zero, Color.White);
+
+            DrawResponses();
+        }
+
+        private void DrawResponses()
+        {
+            if (currentNode.responses == null) return;
+
+            Vector2 startPos = new Vector2(100f, 100f);
+            int lineHeight = font.LineSpacing;
+            int lineNum = 0;
+            int extraLines = 0;
+            foreach (Response response in currentNode.responses)
+            {
+                Vector2 linePos = new Vector2(startPos.X, startPos.Y + ((lineNum + extraLines) * lineHeight));
+                string fmtText = "(" + Keybinds[lineNum] + ") " + response.text;
+                fmtText = WrapText(font, fmtText, screenBounds.Width - linePos.X);
+                // increment lineNum for each carriage return
+                int crCount = fmtText.Split('\n').Length - 1;
+                extraLines = crCount;
+                spriteBatch.DrawString(font, fmtText, linePos, Color.White);
+                lineNum++;
+            }
+        }
+
+        public string WrapText(SpriteFont spriteFont, string text, float maxLineWidth)
+        {
+            string[] words = text.Split(' ');
+
+            StringBuilder sb = new StringBuilder();
+
+            float lineWidth = 0f;
+
+            float spaceWidth = spriteFont.MeasureString(" ").X;
+
+            foreach (string word in words)
+            {
+                Vector2 size = spriteFont.MeasureString(word);
+
+                if (lineWidth + size.X < maxLineWidth)
+                {
+                    sb.Append(word + " ");
+                    lineWidth += size.X + spaceWidth;
+                }
+                else
+                {
+                    sb.Append("\n" + word + " ");
+                    lineWidth = size.X + spaceWidth;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConversationEngine/DialogTree.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace ConversationEngine
+{
+    //-----------------------------------------
+    // DialogNode
+    //  Basic unit of a DialogTree
+    //-----------------------------------------
+    public class DialogNode
+    {
+        public string name;
+        public int curText;
+        public List<string> text;
+        public List<Response> responses;
+        public DialogAction action;
+
+        public DialogNode()
+        {
+            curText = 0;
+        }
+
+        public void NextText() 
+        {
+            if(curText < text.Count - 1)
+                curText++;
+        }
+
+        public string GetCurrentText()
+        {
+            return text[curText];
+        }
+    }
+
+    //-----------------------------------------
+    // DialogAction
+    //  Triggered action in a DialogNode
+    //-----------------------------------------
+    public class DialogAction
+    {
+        public string name;
+        public List<string> strParams;
+
+        public DialogAction(string name, List<string> strParams)
+        {
+            this.name = name;
+            this.strParams = strParams;
+        }
+    }
+
+    //-----------------------------------------
+    // Response
+    //  A single user choice in a DialogNode
+    //-----------------------------------------
+    public class Response
+    {
+        public string text;
+        public string nextNode;
+
+        public Response(string text, string nextNode)
+        {
+            this.text = text;
+            this.nextNode = nextNode;
+        }
+    }
+
+    //-----------------------------------------
+    // DialogTree
+    //  Loads an XML File, stores DialogNodes
+    //-----------------------------------------
+    public class DialogTree
+    {
+        private string name;
+        private List<DialogNode> nodes;
+
+        public DialogTree(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            XmlNode treeNode = doc.GetElementsByTagName("DialogTree")[0];
+
+            name = treeNode.Attributes["name"].Value;
+            Console.WriteLine("Loaded DialogTree '" + name + "'");
+
+            // create each node in file
+            nodes = new List<DialogNode>();
+            foreach (XmlNode node in treeNode.ChildNodes)
+            {
+                DialogNode newDialogNode = new DialogNode();
+                newDialogNode.name = node.Attributes["name"].Value;
+                // get the actions
+                if (node.Attributes["action"] != null)
+                {
+                    string actionName = node.Attributes["action"].Value;
+                    List<string> actionParams = new List<string>();
+                    // TODO: more params
+                    if (node.Attributes["actionParam1"] != null)
+                    {
+                        actionParams.Add(node.Attributes["actionParam1"].Value);
+                    }
+                    newDialogNode.action = new DialogAction(actionName, actionParams);
+                }
+                // get the text and responses
+                foreach (XmlNode child in node.ChildNodes)
+                {
+                    if (child.Name == "Text")
+                    {
+                        if (newDialogNode.text == null)
+                            newDialogNode.text = new List<string>();
+                        newDialogNode.text.Add(child.InnerText);
+                    }
+                    else if (child.Name == "Response")
+                    {
+                        if (newDialogNode.responses == null)
+                            newDialogNode.responses = new List<Response>();
+                        string text = child.InnerText;
+                        string nextNode = child.Attributes["nextNode"].Value;
+                        newDialogNode.responses.Add(new Response(text, nextNode));
+                    }
+                }
+                nodes.Add(newDialogNode);
+            }
+        }
+
+        public DialogNode GetFirstNode()
+        {
+            return nodes[0];
+        }
+
+        public DialogNode GetNodeByName(string nodeName) 
+        {
+            foreach (DialogNode node in nodes)
+            {
+                if (node.name == nodeName)
+                    return node;
+            }
+
+            Console.WriteLine("Node not found!");
+            return null;
+        }
+    }
+}
