171d1d004361fb2853f8cf4d6b2a60b33a4fc577
Charlie Huguenard <charlie.huguenard@gmail.com>
2015-11-03 11:14:25 -0800

    fixed a bug with height and distance

--- OrbitTarget.cs
+++ OrbitTarget.cs
@@ -10,6 +10,7 @@ public class OrbitTarget : MonoBehaviour
     public float HeightDamping = 1f;
     public float RotationDamping = 1f;
     public float LookDamping = 1f;
+    public float AutoOrbitSpeed = 0f;
 
     [SerializeField] private Transform _target;
 
@@ -19,6 +20,8 @@ public class OrbitTarget : MonoBehaviour
         {
             return;
         }
+
+        TargetRotation += AutoOrbitSpeed*Time.deltaTime;
 
         var toMe = transform.position - _target.position;
         var currentDistance = toMe.magnitude;
@@ -30,8 +33,9 @@ public class OrbitTarget : MonoBehaviour
         var newRotation = Mathf.LerpAngle(currentRotation, TargetRotation, Time.deltaTime*RotationDamping);
 
         var newRotationRads = newRotation*Mathf.Deg2Rad;
-        var newRotationDir = new Vector3(Mathf.Cos(newRotationRads), 0f, Mathf.Sin(newRotationRads));
-        transform.position = _target.position + newRotationDir.normalized*newDistance + Vector3.up*newHeight;
+        var newRotationDir = new Vector3(Mathf.Cos(newRotationRads), 0f, Mathf.Sin(newRotationRads)) * newDistance;
+        newRotationDir.y = newHeight;
+        transform.position = _target.position + newRotationDir.normalized*newDistance;
 
         var lookRotation = Quaternion.LookRotation(_target.position - transform.position);
         transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime*LookDamping);
