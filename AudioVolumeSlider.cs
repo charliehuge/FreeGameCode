3f8f350a8c26c9d1abfd59b7debd2ac573a7bfac
Charlie Huguenard <charlie.huguenard@gmail.com>
2016-03-25 18:09:16 -0700

    added audio slider

---
+++ AudioVolumeSlider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A slider that will make audio people happy when working in Unity.
+/// 
+/// Thanks to Aaron, Brad, and Matt for making this: 
+/// http://www.playdotsound.com/portfolio-item/decibel-db-to-float-value-calculator-making-sense-of-linear-values-in-audio-tools/
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class AudioVolumeSlider : MonoBehaviour
+{
+    private const float MinVolume = -100f;
+
+    private const float MaxVolume = 0f;
+
+    [Range(MinVolume, MaxVolume), Tooltip("Volume (dB)")] public float Volume;
+
+    private AudioSource _audioSource;
+
+    private void OnValidate()
+    {
+        if (_audioSource == null)
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        // guard against out of range values
+        if (Volume > MaxVolume)
+        {
+            Volume = 0;
+        }
+        else if (Volume < MinVolume)
+        {
+            Volume = -100f;
+        }
+
+        _audioSource.volume = Mathf.Pow(10f, Volume / 20f);
+    }
+}
